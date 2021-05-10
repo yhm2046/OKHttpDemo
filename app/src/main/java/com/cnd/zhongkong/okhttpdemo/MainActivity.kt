4b2e87255e433742e4a9adced3d56bac07c54971
Michael <yhm2046@gmail.com>
2021-05-10 09:44:34 +0800

    Initial commit

---
+++ app/src/main/java/com/cnd/zhongkong/okhttpdemo/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.cnd.zhongkong.okhttpdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.cnd.zhongkong.okhttpdemo.databinding.ActivityMainBinding
+
+/**
+ * 网络应用demo
+ */
+
+class MainActivity : AppCompatActivity() {
+private val strURL="https://www.google.com/ncr"
+    private val TAG="AppCompatActivity_tag"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        val binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.wbShow.settings.javaScriptEnabled=true
+        binding.wbShow.webViewClient= WebViewClient()
+        binding.wbShow.loadUrl(strURL)
+    }
+}
